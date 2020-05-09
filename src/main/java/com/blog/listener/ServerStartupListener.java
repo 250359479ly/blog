53131056e2fb52c4f78b4e95cecf67f5a5934294
tom_pro <test_pro@qq.com>
2020-05-09 12:21:22 +0800

    test

--- src/main/java/com/blog/listener/ServerStartupListener.java
+++ src/main/java/com/blog/listener/ServerStartupListener.java
@@ -11,12 +11,12 @@ public class ServerStartupListener implements ServletContextListener {
 		// 将web应用名称（路径）保存到application范围中
 		ServletContext application = sce.getServletContext();
 		String path = application.getContextPath();
+		//  /blog
 		application.setAttribute("APP_PATH", path);
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
 
 	}
 
